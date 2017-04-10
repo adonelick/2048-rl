16fd2b655a4b439821b7134590a16a5cd95dd8cc
Andrew Donelick <adonelick@hmc.edu>
2017-04-09 21:43:02 -0600

    Updated the documentation in the 2048 game test

--- play2048.cpp
+++ play2048.cpp
@@ -1,3 +1,8 @@
+/**
+ * Written by Andrew Donelick
+ * EELE 577 - Advanced Digital Signal Processing
+ * Final Project
+ */
 
 #include <iostream>
 #include <stdlib.h>
@@ -7,10 +12,22 @@
 
 using namespace std;
 
+/**
+ * This function is the main function which runs this program. 
+ * In this program, you are able to play a game of 2048 from start
+ * to finish. The game is programmed to stop only when no more moves
+ * remain for the current game state. However, it does notify you of
+ * a win if you reach the 2048 tile.
+ *
+ * :param argc: Number of input arguments
+ * :param argv: Command line arguments
+ *
+ * :return: Error code
+ */
 int main(int argc, char **argv)
 {
 
-    // Initialize the random seed
+    /* Initialize the random seed */
     srand(time(NULL));
 
     /* Print the rules of the game */
@@ -29,6 +46,7 @@ int main(int argc, char **argv)
     unsigned int numActions = game.getActions(actions);
     unsigned int nextAction;
 
+    /* Run the main game loop, as long as there are moves available */
     while (numActions > 0) 
     {
         currentState = game.getState();
@@ -38,6 +56,9 @@ int main(int argc, char **argv)
         cin >> nextAction;
         cout << "" << endl;
 
+        /* Act upon the player's action. Remember the move encoding
+         * as described in the game rules above!
+         */
         if (nextAction == 5) {
             game.takeAction(UP);
         } else if (nextAction == 2) {
@@ -57,6 +78,8 @@ int main(int argc, char **argv)
             cout << "You Win!" << endl;
         }
     }
+
+    cout << "No more moves. Game over!" << endl;
 
     return 0;
 }
